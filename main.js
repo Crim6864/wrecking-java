8b2b0375773b4137ce8851504b5a75c190d0be98
Crim6864 <daniel.erikssonb@gmail.com>
2024-03-30 08:55:46 +0000

    added window limiters

--- main.js
+++ main.js
@@ -63,34 +63,12 @@ function createBricks() {
     }
 }
 
-// Functions for touch control
-function touchStartHandler(e) {
-    e.preventDefault();
-    var touch = e.touches[0];
-    if (touch.pageX > canvas.width / 2) {
-        rightPressed = true;
-    } else {
-        leftPressed = true;
-    }
-}
-
-function touchEndHandler(e) {
-    e.preventDefault();
-    rightPressed = false;
-    leftPressed = false;
-}
-
-function touchMoveHandler(e) {
-    e.preventDefault();
-    var touch = e.touches[0];
-    if (touch.pageX > canvas.width / 2) {
-        rightPressed = true;
-        leftPressed = false;
-    } else {
-        leftPressed = true;
-        rightPressed = false;
-    }
-}
+// Event listeners for paddle control
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+canvas.addEventListener("touchstart", touchStartHandler, false);
+canvas.addEventListener("touchend", touchEndHandler, false);
+canvas.addEventListener("touchmove", touchMoveHandler, false);
 
 // Functions for paddle control
 function keyDownHandler(e) {
