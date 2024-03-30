548a33e69ad36b4b4e7230d456165ef76d01c602
Crim6864 <daniel.erikssonb@gmail.com>
2024-03-30 11:04:15 +0000

    fixing bricks

--- game.js
+++ game.js
@@ -4,70 +4,53 @@ const ctx = canvas.getContext('2d');
 const ballRadius = 20;
 const paddleHeight = 10;
 const paddleWidth = 75;
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
-let paddleX = (canvas.width - paddleWidth) / 2;
+const brickRowCount = 5;
+const brickColumnCount = 8;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+let bricks = [];
 
-// Touch Controls
-let touchStartX = 0;
-let touchEndX = 0;
-
-canvas.addEventListener('touchstart', touchStart, false);
-canvas.addEventListener('touchend', touchEnd, false);
-
-function touchStart(event) {
-    touchStartX = event.changedTouches[0].screenX;
-}
-
-function touchEnd(event) {
-    touchEndX = event.changedTouches[0].screenX;
-    handleTouchControls();
-}
-
-function handleTouchControls() {
-    const touchDiff = touchEndX - touchStartX;
-    if (touchDiff > 0) {
-        // Swipe right
-        paddleX += 10;
-    } else {
-        // Swipe left
-        paddleX -= 10;
+// Initialize Bricks
+for (let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 }; // status 1 means the brick is active
     }
 }
 
-// Draw Ball
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = '#0095DD';
-    ctx.fill();
-    ctx.closePath();
+// Draw Bricks
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = '#0095DD';
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
 }
 
-// Draw Paddle
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = '#0095DD';
-    ctx.fill();
-    ctx.closePath();
-}
-
-// Collision Detection
+// Collision Detection with Bricks
 function collisionDetection() {
-    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if (y + dy < ballRadius) {
-        dy = -dy;
-    } else if (y + dy > canvas.height - ballRadius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy;
-        } else {
-            alert('Game Over');
-            document.location.reload();
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const brick = bricks[c][r];
+            if (brick.status === 1) {
+                if (x > brick.x && x < brick.x + brickWidth && y > brick.y && y < brick.y + brickHeight) {
+                    dy = -dy;
+                    brick.status = 0; // Set brick status to 0, meaning it's destroyed
+                }
+            }
         }
     }
 }
@@ -77,7 +60,14 @@ function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawBricks();
     collisionDetection();
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
 
     x += dx;
     y += dy;
