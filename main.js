69fc881e2166faa67132f3602a6226d13a4dbbb8
Crim6864 <daniel.erikssonb@gmail.com>
2024-03-30 10:15:42 +0000

    fixing bricks

--- main.js
+++ main.js
@@ -178,26 +178,6 @@ function drawPaddle() {
     ctx.closePath();
 }
 
-// Draw the bricks
-function drawBricks() {
-    for (var c = 0; c < brickColumnCount; c++) {
-        for (var r = 0; r < brickRowCount; r++) {
-            if (bricks[c][r].status == 1) {
-                var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-                var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
-                ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#0095DD";
-                ctx.fill();
-                ctx.closePath();
-            }
-        }
-    }
-}
-
-
 // Draw the score and lives
 function drawScore() {
     ctx.font = "16px Arial";
